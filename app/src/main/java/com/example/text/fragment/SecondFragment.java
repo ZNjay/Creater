55e68d8a7aee3dc37ccecf7a33fe541a1116f958
Admin <123>
2023-02-14 17:55:33 +0800

    第二个碎片—通讯录

--- app/src/main/java/com/example/text/fragment/SecondFragment.java
+++ app/src/main/java/com/example/text/fragment/SecondFragment.java
@@ -1,16 +1,24 @@
 package com.example.text.fragment;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 
 import android.app.Fragment;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
 import com.example.text.R;
@@ -18,7 +26,7 @@ import com.example.text.R;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressLint("ValidFragment")
+
 public class SecondFragment extends Fragment {
 
 
@@ -30,8 +38,61 @@ public class SecondFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.secondfragment, container, false);
-
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ListView contactsView = getActivity().findViewById(R.id.listview);
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, contactList);
+        contactsView.setAdapter(adapter);
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{
+                    Manifest.permission.READ_CONTACTS},1);
+        } else {
+            readContacts();
+        }
+    }
+
+
+
+    private void readContacts() {
+        Cursor cursor = null;
+        try {
+            //查询联系人数据
+            cursor = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            if (cursor != null) {
+                while (cursor.moveToNext()) {
+                    //获取联系人姓名
+                    @SuppressLint("Range") String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    //获取联系人手机号
+                    @SuppressLint("Range") String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    contactList.add(displayName+"\n"+number);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    readContacts();
+                } else {
+                    Toast.makeText(getActivity(), "没有权限", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
     }
 
 }
